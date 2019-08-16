cf0899b0ad86efea026a5b21b317d1d7616b5293
pengtao.qiu <qptkk@163.com>
2019-08-16 14:42:46 +0800

    [chore]: format codes

--- buddy2.h
+++ buddy2.h
@@ -4,13 +4,17 @@
 #include <stdlib.h>
 
 struct buddy2;
-struct buddy2* buddy2_new( int size );
-void buddy2_destroy( struct buddy2* self );
 
-int buddy2_alloc(struct buddy2* self, int size);
-void buddy2_free(struct buddy2* self, int offset);
+struct buddy2 *buddy2_new(unsigned int size);
 
-int buddy2_size(struct buddy2* self, int offset);
-void buddy2_dump(struct buddy2* self);
+void buddy2_destroy(struct buddy2 *self);
+
+unsigned int buddy2_alloc(struct buddy2 *self, unsigned int size);
+
+void buddy2_free(struct buddy2 *self, unsigned int offset);
+
+unsigned int buddy2_size(struct buddy2 *self, unsigned int offset);
+
+void buddy2_dump(struct buddy2 *self);
 
 #endif//__BUDDY2_H__
--- buddy2_test.c
+++ buddy2_test.c
@@ -1,22 +1,28 @@
 #include "buddy2.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
 int main() {
-  char cmd[80];
-  int arg;
-  struct buddy2* buddy = buddy2_new(32);
-  buddy2_dump(buddy);
-  for (;;) {
-    scanf("%s %d", cmd, &arg);
-    if (strcmp(cmd, "alloc") == 0) {
-      printf("allocated@%d\n", buddy2_alloc(buddy, arg));
-      buddy2_dump(buddy);
-    } else if (strcmp(cmd, "free") == 0) {
-      buddy2_free(buddy, arg);
-      buddy2_dump(buddy);
-    } else if (strcmp(cmd, "size") == 0) {
-      printf("size: %d\n", buddy2_size(buddy, arg));
-      buddy2_dump(buddy);
-    } else
-      buddy2_dump(buddy);
-  }
+    char cmd[80];
+    int arg;
+    struct buddy2 *buddy = buddy2_new(32);
+    buddy2_dump(buddy);
+    while (true) {
+        scanf("%s %d", cmd, &arg);
+        if (strcmp(cmd, "alloc") == 0) {
+            printf("allocated@%d\n", buddy2_alloc(buddy, arg));
+            buddy2_dump(buddy);
+        } else if (strcmp(cmd, "free") == 0) {
+            buddy2_free(buddy, arg);
+            buddy2_dump(buddy);
+        } else if (strcmp(cmd, "size") == 0) {
+            printf("size: %d\n", buddy2_size(buddy, arg));
+            buddy2_dump(buddy);
+        } else if (strcmp(cmd, "exit") == 0) {
+            return 0;
+        } else {
+            buddy2_dump(buddy);
+        }
+    }
 }
