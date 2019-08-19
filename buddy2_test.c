1ba4a8216398ebfb546e0c9042ec14892159e64e
pengtao.qiu <qptkk@163.com>
2019-08-19 15:04:31 +0800

    [chore]: format codes

--- buddy2_test.c
+++ buddy2_test.c
@@ -5,19 +5,19 @@
 
 int main() {
     char cmd[80];
-    int arg;
+    int size;
     struct buddy2* buddy = buddy2_new(32); // 2^5
     buddy2_dump(buddy);
     while (true) {
-        scanf("%s %d", cmd, &arg);
+        scanf("%s %d", cmd, &size);
         if (strcmp(cmd, "alloc") == 0) {
-            printf("allocated@%d\n", buddy2_alloc(buddy, arg));
+            printf("allocated@%d\n", buddy2_alloc(buddy, size));
             buddy2_dump(buddy);
         } else if (strcmp(cmd, "free") == 0) {
-            buddy2_free(buddy, arg);
+            buddy2_free(buddy, size);
             buddy2_dump(buddy);
         } else if (strcmp(cmd, "size") == 0) {
-            printf("size: %d\n", buddy2_size(buddy, arg));
+            printf("size: %d\n", buddy2_size(buddy, size));
             buddy2_dump(buddy);
         } else if (strcmp(cmd, "exit") == 0) {
             buddy2_destroy(buddy);
